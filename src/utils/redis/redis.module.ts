6492e1da91ed0674bf96b541301df13ead379b34
amitturare <amitturare@outlook.com>
2025-06-29 12:09:54 +0530

    refactor(redis): commenting out unused code
    - Commented out Redis provider configuration and related imports in redis.module.ts to simplify the module structure.
    - This change prepares the module for potential future adjustments while maintaining clarity in the codebase.

--- src/utils/redis/redis.module.ts
+++ src/utils/redis/redis.module.ts
@@ -1,33 +1,32 @@
 import { Module } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import Redis from 'ioredis';
+// import { ConfigService } from '@nestjs/config';
+// import Redis from 'ioredis';
 
-import { RedisService } from './redis.service';
+// import { RedisService } from './redis.service';
 
 @Module({
-  providers: [
-    {
-      provide: 'REDIS',
-      inject: [ConfigService],
-      useFactory: () => {
-        const redis = new Redis({
-          // host: configService.getOrThrow<string>(ENV_KEYS.REDIS_HOST),
-          // port: +configService.getOrThrow<string>(ENV_KEYS.REDIS_PORT),
-          // username: configService.get<string>(ENV_KEYS.REDIS_USERNAME),
-          // password: configService.get<string>(ENV_KEYS.REDIS_PASSWORD),
-          connectTimeout: 10000,
-          retryStrategy: (times: number) => {
-            const delay = Math.min(times * 1000, 5000);
-            return delay;
-          },
-          maxRetriesPerRequest: 3,
-        });
-
-        return redis;
-      },
-    },
-    RedisService,
-  ],
-  exports: ['REDIS', RedisService],
+  // providers: [
+  //   {
+  //     provide: 'REDIS',
+  //     inject: [ConfigService],
+  //     useFactory: () => {
+  //       const redis = new Redis({
+  //         // host: configService.getOrThrow<string>(ENV_KEYS.REDIS_HOST),
+  //         // port: +configService.getOrThrow<string>(ENV_KEYS.REDIS_PORT),
+  //         // username: configService.get<string>(ENV_KEYS.REDIS_USERNAME),
+  //         // password: configService.get<string>(ENV_KEYS.REDIS_PASSWORD),
+  //         connectTimeout: 10000,
+  //         retryStrategy: (times: number) => {
+  //           const delay = Math.min(times * 1000, 5000);
+  //           return delay;
+  //         },
+  //         maxRetriesPerRequest: 3,
+  //       });
+  //       return redis;
+  //     },
+  //   },
+  //   RedisService,
+  // ],
+  // exports: ['REDIS', RedisService],
 })
 export class RedisModule {}
